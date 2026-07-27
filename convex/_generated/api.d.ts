f2555ac3df07e31f6938e178df0af1ec90a3d7e6
Yunus Ahmed Choudhury <yunuschoudhurywork@gmail.com>
2026-07-27 21:27:03 +0600

    Created metrics services

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as metrics from "../metrics.js";
 import type * as user from "../user.js";
 
 import type {
@@ -17,6 +18,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  metrics: typeof metrics;
   user: typeof user;
 }>;
 
