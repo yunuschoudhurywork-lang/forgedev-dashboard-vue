edd9089de6ea3a122c0eaa3fbc73f06f6d5afb2b
Yunus Ahmed Choudhury <yunuschoudhurywork@gmail.com>
2026-08-05 00:00:39 +0600

    Define App Routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router"
+
+import MetricsDashboard from "@/pages/MetricsDashboard.vue"
+import ClientMetricsDetail from "@/pages/ClientMetricsDetail.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+
+  routes: [
+    {
+      path: "/",
+      redirect: "/metrics",
+    },
+    {
+      path: "/metrics",
+      name: "metrics-dashboard",
+      component: MetricsDashboard,
+    },
+    {
+      path: "/metrics/:clientId",
+      name: "client-metrics-detail",
+      component: ClientMetricsDetail,
+      props: true,
+    },
+  ],
 })
 
 export default router
